4d1b018e86c25c3cd2795471e1eeffa9c4de5464
fata72 <chausette74@hotmail.fr>
2019-11-20 19:04:10 +0100

    classe test login #1

---
+++ src/test/java/geco/LoginServiceTest.groovy
@@ -0,0 +1,19 @@
+package geco
+
+class LoginServiceTest extends groovy.util.GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+
+    void testLoginExists() {
+    }
+
+    void testAddLogin() {
+    }
+
+    void testFindAllLoginsStartingWith() {
+    }
+
+    void testFindAllLogins() {
+    }
+}
