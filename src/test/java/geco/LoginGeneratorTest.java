2424152749c9e8e6ea496c6850a1f716d262cd81
fata72 <chausette74@hotmail.fr>
2019-11-26 11:13:34 +0100

    premier test de testLoginGenerator #2

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,25 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginGenerator lg;
+    private LoginService ls;
+
+    @Before
+    public void setUp() throws Exception {
+        ls = new LoginService( new String[] {"JROL", "BPER", "CGUR", "JDU", "JRAL", "JRAL1"} );
+        lg = new LoginGenerator(ls);
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom() {
+        lg.generateLoginForNomAndPrenom("Durand","Paul");
+        assertTrue(ls.loginExists("PDUR"));
+    }
+
+}
